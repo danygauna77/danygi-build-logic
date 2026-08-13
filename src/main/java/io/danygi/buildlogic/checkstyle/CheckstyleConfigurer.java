fc56b1f4b9df7a6b1ad4dded07067e0538cb50a5
DESKTOP-4OOQTCH\54922 <danygauna77@gmail.com>
2026-08-13 08:43:29 -0300

    feat: centralize quality plugin configuration

--- src/main/java/io/danygi/buildlogic/checkstyle/CheckstyleConfigurer.java
+++ src/main/java/io/danygi/buildlogic/checkstyle/CheckstyleConfigurer.java
@@ -1,6 +1,7 @@
 package io.danygi.buildlogic.checkstyle;
 
 import org.gradle.api.Project;
+import org.gradle.api.Task;
 import org.gradle.api.plugins.quality.Checkstyle;
 import org.gradle.api.plugins.quality.CheckstyleExtension;
 
@@ -11,28 +12,56 @@ import java.nio.file.StandardCopyOption;
 
 public final class CheckstyleConfigurer {
 
+    private static final String CHECKSTYLE_TASK_NAME = "copyDanygiCheckstyleConfig";
+    private static final String CHECKSTYLE_CONFIG_PATH = "danygi/checkstyle.xml";
+
     private CheckstyleConfigurer() {
     }
 
     public static void configure(Project project) {
         project.getPluginManager().apply("checkstyle");
 
+        File configFile = getCheckstyleConfigFile(project);
+
+        Task copyCheckstyleConfig = project.getTasks().register(
+                CHECKSTYLE_TASK_NAME,
+                task -> configureCopyTask(task, configFile)
+        ).get();
+
         CheckstyleExtension extension =
                 project.getExtensions().getByType(CheckstyleExtension.class);
 
         extension.setToolVersion("10.26.1");
         extension.setIgnoreFailures(false);
-        extension.setConfigFile(getCheckstyleConfig(project));
+        extension.setConfigFile(configFile);
 
         project.getTasks()
                 .withType(Checkstyle.class)
                 .configureEach(task -> {
+                    task.dependsOn(copyCheckstyleConfig);
+
                     task.getReports().getHtml().getRequired().set(true);
                     task.getReports().getXml().getRequired().set(true);
                 });
     }
 
-    private static File getCheckstyleConfig(Project project) {
+    private static File getCheckstyleConfigFile(Project project) {
+        return new File(
+                project.getLayout()
+                        .getBuildDirectory()
+                        .get()
+                        .getAsFile(),
+                CHECKSTYLE_CONFIG_PATH
+        );
+    }
+
+    private static void configureCopyTask(Task task, File configFile) {
+        task.getOutputs().file(configFile);
+
+        task.doLast(ignored -> copyCheckstyleConfig(configFile));
+    }
+
+    private static void copyCheckstyleConfig(File configFile) {
         try {
             var resource = CheckstyleConfigurer.class
                     .getClassLoader()
@@ -44,27 +73,19 @@ public final class CheckstyleConfigurer {
                 );
             }
 
-            File configFile = new File(
-                    project.getLayout()
-                            .getBuildDirectory()
-                            .get()
-                            .getAsFile(),
-                    "danygi/checkstyle.xml"
-            );
+            try (resource) {
+                Files.createDirectories(configFile.getParentFile().toPath());
 
-            Files.createDirectories(configFile.getParentFile().toPath());
-
-            Files.copy(
-                    resource,
-                    configFile.toPath(),
-                    StandardCopyOption.REPLACE_EXISTING
-            );
-
-            return configFile;
+                Files.copy(
+                        resource,
+                        configFile.toPath(),
+                        StandardCopyOption.REPLACE_EXISTING
+                );
+            }
 
         } catch (IOException e) {
             throw new RuntimeException(
-                    "Unable to load Checkstyle configuration",
+                    "Unable to copy Checkstyle configuration",
                     e
             );
         }
